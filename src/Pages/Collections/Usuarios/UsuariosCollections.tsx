7d757f604a88ec16fd42fc9552d5ee51cc41fd07
Ricardo Zapata <rishiido@gmail.com>
2024-05-07 23:47:49 -0600

    Modal to add users

--- src/Pages/Collections/Usuarios/UsuariosCollections.tsx
+++ src/Pages/Collections/Usuarios/UsuariosCollections.tsx
@@ -1,15 +1,47 @@
-import { Space, Typography } from 'antd'
-import React from 'react'
-import UsersTable from '../../../Components/UserTables'
+import { Button, Modal, Space, Typography } from 'antd'
+import React, { useState } from 'react';
+import UsersTable from '../../../components/UserTables';
 
 
 
 const UsuariosCollections:React.FC = () => {
+  const [open, setOpen] = useState(false);
+  const [confirmLoading, setConfirmLoading] = useState(false);
+  const [modalText, setModalText] = useState('Content of the modal');
+
+  const handleOk = () => {
+    setModalText('The modal will be closed after two seconds');
+    setConfirmLoading(true);
+    setTimeout(() => {
+      setOpen(false);
+      setConfirmLoading(false);
+      setModalText('Content of the modal')
+    }, 2000);
+  };
+
+  const handleCancel = () => {
+    console.log('Clicked cancel button');
+    setOpen(false);
+  };
   return (
-    <Space size={'large'} align={'start'} direction={'vertical'}>
+    <Space size={'large'} align={'start'} direction={'vertical'} style={{width: '100%'}}>
         <Typography.Title>Usuarios</Typography.Title>
+              
+        <Button type="primary" onClick={()=>setOpen(true)} style={{ marginBottom: 16 }}>
+          Add a row
+        </Button>
         
             <UsersTable />
+
+            <Modal
+        title="Title"
+        open={open}
+        onOk={handleOk}
+        confirmLoading={confirmLoading}
+        onCancel={handleCancel}
+      >
+        <p>{modalText}</p>
+      </Modal>
         
     </Space>
   )
--- src/components/UserTables.tsx
+++ src/components/UserTables.tsx
@@ -1,11 +1,12 @@
-import { Table } from "antd";
+import { Button, Col, Modal, Row, Table } from "antd";
 import { useEffect, useState } from "react";
 import { Product } from "../models/product";
 import { getProducts } from "../services/product";
+import { ExclamationCircleFilled } from "@ant-design/icons";
 
 const UsersTable: React.FC = () => {
 	const [products, setProducts] = useState<Product[]>([]);
-
+  const { confirm } = Modal;
 	useEffect(() => {
 		const fetchProducts = async () => {
 			try {
@@ -30,12 +31,43 @@ const UsersTable: React.FC = () => {
 			dataIndex: "price",
 			key: "price",
 		},
+    {
+			title: "Name",
+			dataIndex: "name",
+			key: "name",
+		},
+		{
+			title: "Price",
+			dataIndex: "price",
+			key: "price",
+		},
 	];
+
+  const deleteElement = () => {
+    confirm({
+      title: 'Do you want to delete these items?',
+      icon: <ExclamationCircleFilled />,
+      content: 'When clicked the OK button, this dialog will be closed after 1 second',
+      onOk() {
+        return new Promise((resolve, reject) => {
+          setTimeout(Math.random() > 0.5 ? resolve : reject, 1000);
+        }).catch(() => console.log('Oops errors!'));
+      },
+      onCancel() {},
+    });
+  };
 
 	return (
 		<>
-			<Table columns={columns} dataSource={products} size={'large'} style={{ width: '100%' }}/>
-		</>
+
+
+        <Table
+          columns={columns}
+          dataSource={products}
+          size="large"
+          style={{ width: '100%' }}
+        />
+    </>
 	);
 };
 
